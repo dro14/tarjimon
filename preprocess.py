89df8acedb104b862b4611cfd6264296cd4b26cf
dro14 <doniyorbek752@gmail.com>
2024-04-29 15:15:00 +0500

    refactor preprocess() and translate()

--- preprocess.py
+++ preprocess.py
@@ -1,13 +1,16 @@
 from functions.quote_standardization import quote_standardization
 from functions.sentence_split import sentence_split
 from functions.strip_replace import strip_replace
+from functions.extract_urls import extract_urls
 
 
-def preprocess(s: str) -> list[str]:
+def preprocess(s: str) -> tuple[list[str], list[list[str]]]:
     s = strip_replace(s)
-    sentences = []
+    sentences, links = [], []
     for sentence in sentence_split(s):
         sentence = quote_standardization(sentence)
+        sentence, urls = extract_urls(sentence)
         sentence = "translate English to Uzbek: " + sentence
         sentences.append(sentence)
-    return sentences
+        links.append(urls)
+    return sentences, links
--- translate.py
+++ translate.py
@@ -1,5 +1,4 @@
 from transformers import T5Tokenizer, TFT5ForConditionalGeneration
-from functions.extract_urls import extract_urls
 from preprocess import preprocess
 
 tokenizer = T5Tokenizer.from_pretrained("model")
@@ -11,15 +10,12 @@ def translate(s: str) -> str:
     for i, line in enumerate(lines):
         if not line.strip():
             continue
-        sentences = preprocess(line)
+        sentences, urls = preprocess(line)
+        inputs = tokenizer.encode(sentences, max_length=128, padding="max_length", truncation=True)
+        outputs = model.generate(inputs, max_length=128)
+        sentences = tokenizer.decode(outputs, skip_special_tokens=True)
         for j, sentence in enumerate(sentences):
-            sentence, urls = extract_urls(sentence)
-            input_ids = tokenizer.encode(sentence, max_length=128, padding="max_length", truncation=True)
-            outputs = model.generate([input_ids], max_length=128)
-            sentence = tokenizer.decode(outputs[0], skip_special_tokens=True)
-            for url in urls:
-                sentence = sentence.replace("URL", url, 1)
-            sentences[j] = sentence
-            j += 1
+            for url in urls[j]:
+                sentences[j] = sentence.replace("URL", url, 1)
         lines[i] = " ".join(sentences)
     return "\n".join(lines)
