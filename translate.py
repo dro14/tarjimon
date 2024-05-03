e628883b273042f5f2b3ce5be513b191369df04f
dro14 <doniyorbek752@gmail.com>
2024-05-03 17:51:57 +0500

    fix bug with max_new_tokens

--- translate.py
+++ translate.py
@@ -2,20 +2,17 @@ from functions.quote_standardization import quote_standardization
 from functions.strip_replace import strip_replace
 from functions.extract_urls import extract_urls
 from huggingface_hub import InferenceClient
-from transformers import T5Tokenizer
 import os
 
-tokenizer = T5Tokenizer.from_pretrained("dro14/tarjimon")
 client = InferenceClient(model="dro14/tarjimon", token=os.environ["HF_TOKEN"])
 
 
 def translate(text: str) -> str:
-    max_new_tokens = 512 - len(tokenizer.tokenize(text))
     text = strip_replace(text)
     text = quote_standardization(text)
     text, urls = extract_urls(text)
     text = "translate English to Uzbek: " + text
-    text = client.text_generation(text, max_new_tokens=max_new_tokens)
+    text = client.text_generation(text, max_new_tokens=250)
     for url in urls:
         text = text.replace("URL", url, 1)
     return text
