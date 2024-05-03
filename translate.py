fb5e7483b53593afdd0c9a0f943738a4f4df3dcd
dro14 <doniyorbek752@gmail.com>
2024-05-03 17:40:54 +0500

    remove splitting into sentences

--- translate.py
+++ translate.py
@@ -1,15 +1,21 @@
+from functions.quote_standardization import quote_standardization
+from functions.strip_replace import strip_replace
+from functions.extract_urls import extract_urls
 from huggingface_hub import InferenceClient
-from preprocess import preprocess
+from transformers import T5Tokenizer
 import os
 
+tokenizer = T5Tokenizer.from_pretrained("dro14/tarjimon")
 client = InferenceClient(model="dro14/tarjimon", token=os.environ["HF_TOKEN"])
 
 
-def translate(s: str) -> str:
-    sentences, urls = preprocess(s)
-    for i, sentence in enumerate(sentences):
-        sentence = client.text_generation(sentence, max_new_tokens=128)
-        for url in urls[i]:
-            sentence = sentence.replace("URL", url, 1)
-        sentences[i] = sentence
-    return " ".join(sentences)
+def translate(text: str) -> str:
+    max_new_tokens = 512 - len(tokenizer.tokenize(text))
+    text = strip_replace(text)
+    text = quote_standardization(text)
+    text, urls = extract_urls(text)
+    text = "translate English to Uzbek: " + text
+    text = client.text_generation(text, max_new_tokens=max_new_tokens)
+    for url in urls:
+        text = text.replace("URL", url, 1)
+    return text
