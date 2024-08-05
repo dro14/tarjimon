8f91cd0be6587bf667463fb9bbbf04ff7435bf4e
dro14 <doniyorbek752@gmail.com>
2024-08-05 12:44:56 +0500

    add beam search decoding

--- translate.py
+++ translate.py
@@ -8,7 +8,9 @@ model = TFT5ForConditionalGeneration.from_pretrained("dro14/tarjimon")
 def translate(s: str) -> str:
     sentences, urls = preprocess(s)
     inputs = tokenizer(sentences, padding=True)
-    outputs = model.generate(inputs["input_ids"], max_length=256)
+    outputs = model.generate(
+        inputs["input_ids"], max_length=256,
+        num_beams=5, do_sample=False, no_repeat_ngram_size=2)
     for j, output in enumerate(outputs):
         sentence = tokenizer.decode(output, skip_special_tokens=True)
         for url in urls[j]:
